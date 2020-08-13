e1a7120a3295455d0f21d917b7a4a10f557c1a8e
Austin Cummings <aac729@live.com>
2020-08-13 01:11:55 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class Author
+  attr_accessor :name
+
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    #binding.pry 
+    Post.all.select do |post|
+      post.author == self
+    end 
+  end
+
+  def self.post_count
+   Post.all.count 
+  end
+
+  def add_post(post_title)
+    @posts << post_title
+    post_title.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+end
